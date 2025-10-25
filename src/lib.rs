56df63e60c9fe8cf0c6dc7d429defc30219b3ebb
A. Taha Baki <atahabaki.dev@pm.me>
2025-10-25 12:21:35 +0300

    mv: _explode -> explosion, organize tests

--- src/lib.rs
+++ src/lib.rs
@@ -6,15 +6,15 @@ pub(crate) mod flag;
 pub(crate) mod parser;
 pub(crate) mod tokenizer;
 
-pub fn explode<'a>(data: &'a str) -> Result<Vec<String>, Box<dyn Error>> {
-    _explode(data, Flag::default())
+pub fn explode(data: &str) -> Result<Vec<String>, Box<dyn Error>> {
+    explosion(data, Flag::default())
 }
 
-pub fn explode_with_flags<'a>(data: &'a str, flags: Flag) -> Result<Vec<String>, Box<dyn Error>> {
-    _explode(data, flags)
+pub fn explode_with_flags(data: &str, flags: Flag) -> Result<Vec<String>, Box<dyn Error>> {
+    explosion(data, flags)
 }
 
-fn _explode<'a>(data: &'a str, flags: Flag) -> Result<Vec<String>, Box<dyn Error>> {
+fn explosion(data: &str, flags: Flag) -> Result<Vec<String>, Box<dyn Error>> {
     let tokenizer = Tokenizer::new(data, flags);
     let tokens = tokenizer.tokenize()?;
     let parser = Parser::from_tokenizer(tokenizer, tokens);
@@ -31,7 +31,7 @@ mod tests {
         assert_eq!(
             explode("{A,B}").unwrap(),
             vec!["A".to_string(), "B".to_string()]
-        )
+        );
     }
 
     #[test]
@@ -47,7 +47,7 @@ mod tests {
                 "F".to_string(),
                 "G".to_string()
             ]
-        )
+        );
     }
 
     #[test]
@@ -55,55 +55,57 @@ mod tests {
         assert_eq!(
             explode("{1..3}").unwrap(),
             vec!["1".to_string(), "2".to_string(), "3".to_string()]
-        )
+        );
     }
 
-    #[test]
     #[cfg(feature = "range_padding")]
-    fn range_padding_zero_90_to_100() {
-        assert_eq!(
-            explode("{90..100;0=}").unwrap(),
-            vec![
-                "090".to_string(),
-                "091".to_string(),
-                "092".to_string(),
-                "093".to_string(),
-                "094".to_string(),
-                "095".to_string(),
-                "096".to_string(),
-                "097".to_string(),
-                "098".to_string(),
-                "099".to_string(),
-                "100".to_string(),
-            ]
-        )
-    }
+    mod range_padding {
+        use super::*;
 
-    #[test]
-    #[cfg(feature = "range_padding")]
-    fn range_padding_zero_9_to_10() {
-        assert_eq!(
-            explode("{7..10;100_00=}").unwrap(),
-            vec![
-                "100_007".to_string(),
-                "100_008".to_string(),
-                "100_009".to_string(),
-                "100_010".to_string(),
-            ]
-        )
-    }
+        #[test]
+        fn range_padding_zero_90_to_100() {
+            assert_eq!(
+                explode("{90..100;0=}").unwrap(),
+                vec![
+                    "090".to_string(),
+                    "091".to_string(),
+                    "092".to_string(),
+                    "093".to_string(),
+                    "094".to_string(),
+                    "095".to_string(),
+                    "096".to_string(),
+                    "097".to_string(),
+                    "098".to_string(),
+                    "099".to_string(),
+                    "100".to_string(),
+                ]
+            );
+        }
 
-    #[test]
-    #[cfg(feature = "range_padding")]
-    fn range_padding_zero_10_to_7() {
-        assert_eq!(
-            explode("{10..7;=00_000}").unwrap(),
-            vec![
-                "700_000".to_string(),
-                "800_000".to_string(),
-                "900_000".to_string(),
-                "100_000".to_string(),
-            ]
-        )
+        #[test]
+        fn range_padding_zero_9_to_10() {
+            assert_eq!(
+                explode("{7..10;100_00=}").unwrap(),
+                vec![
+                    "100_007".to_string(),
+                    "100_008".to_string(),
+                    "100_009".to_string(),
+                    "100_010".to_string(),
+                ]
+            );
+        }
+
+        #[test]
+        fn range_padding_zero_10_to_7() {
+            assert_eq!(
+                explode("{10..7;=00_000}").unwrap(),
+                vec![
+                    "700_000".to_string(),
+                    "800_000".to_string(),
+                    "900_000".to_string(),
+                    "100_000".to_string(),
+                ]
+            );
+        }
     }
 }
